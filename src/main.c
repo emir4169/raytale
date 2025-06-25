39ab82d48fa15bcd111ce2040fcf9e46e23a0330
emir4169 <em.ireri41@gmail.com>
2025-06-25 10:47:13 +0300

    turn stuff

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 */
 
 #include "raylib.h"
-
+#include <stdio.h>
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 #define ARENA_OFFSET_X 35
 #define ARENA_OFFSET_Y 253
@@ -27,7 +27,10 @@ Vector2 ButtonPos[4] = {
 };
 int button_selected = 0; // 0 = fight, 1 = act, 2 = item, 3 = mercy
 Vector2 SoulPos = { 200+253, 50+35 }; // position of the soul in the arena
-
+int SoulSpeed = 4;
+bool isSlow = false; // X button thingamajing
+typedef enum BattleStateEnum { PLAYER = 0, ENEMY } BattleStateEnum; // this stores the core state of the battle
+BattleStateEnum BattleState = PLAYER;
 void DrawShape(Vector2 *shape, int numPoints, Color color)
 {
 	for (int i = 0; i < numPoints; i++)
@@ -41,7 +44,7 @@ int main ()
 {
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
+	SetTargetFPS(30);
 	// Create the window and OpenGL context
 	InitWindow(640, 480, "Hello Raylib");																																																																																																							// end the frame and get ready for the next one  (displ */
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
@@ -62,15 +65,52 @@ int main ()
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
 		// update
-		if (IsKeyPressed(KEY_RIGHT))
-		{
-			button_selected++;
-			if (button_selected > 3) button_selected = 0; // wrap around
+		if (IsKeyDown(KEY_F1)){
+			printf(TextFormat("POS: %d, %d", SoulPos.x, SoulPos.y));
 		}
-		else if (IsKeyPressed(KEY_LEFT))
+		if (IsKeyPressed(KEY_ENTER)){
+			BattleState = (BattleState == PLAYER) ? ENEMY : PLAYER; // toggle between player and enemy state
+		}
+		switch (BattleState)
 		{
-			button_selected--;
-			if (button_selected < 0) button_selected = 3; // wrap around
+		case PLAYER:
+			if (IsKeyPressed(KEY_RIGHT))	
+			{
+				button_selected++;
+				if (button_selected > 3) button_selected = 0; // wrap around
+			}
+			else if (IsKeyPressed(KEY_LEFT))
+			{
+				button_selected--;
+				if (button_selected < 0) button_selected = 3; // wrap around
+			}
+			break;
+		case ENEMY:
+			if (IsKeyDown(KEY_X))
+			{
+				isSlow = true;
+			} else if (IsKeyUp(KEY_X))
+			{
+				isSlow = false;
+			}
+			if (IsKeyDown(KEY_RIGHT))
+			{
+				SoulPos.x += SoulSpeed/(isSlow ? 2 : 1);
+			}
+			if (IsKeyDown(KEY_LEFT))
+			{
+				SoulPos.x -= SoulSpeed/(isSlow ? 2 : 1);
+			}
+			if (IsKeyDown(KEY_UP))
+			{
+				SoulPos.y -= SoulSpeed/(isSlow ? 2 : 1);
+			}
+			if (IsKeyDown(KEY_DOWN))
+			{	
+				SoulPos.y += SoulSpeed/(isSlow ? 2 : 1);
+			}
+		default:
+			break;
 		}
 
 		// drawing
@@ -86,37 +126,49 @@ int main ()
 
 		// draw some text using the default font
 		DrawText("Hello Raylib", 200,200,20,WHITE);
+		DrawText(TextFormat("BattleState: %d", BattleState), 10, 10, 20, WHITE);
 		// draw our texture to the screen
-		switch (button_selected)
+		switch (BattleState)
 		{
-		case 0:
-			DrawTextureV(fightbtt_selected, ButtonPos[0], WHITE);
-			DrawTextureV(actbtt,   			ButtonPos[1], WHITE);
-			DrawTextureV(itembtt,  			ButtonPos[2], WHITE);
-			DrawTextureV(mercybtt, 			ButtonPos[3], WHITE);
+		case PLAYER:
+			switch (button_selected)
+			{
+				case 0:
+					DrawTextureV(fightbtt_selected, ButtonPos[0], WHITE);
+					DrawTextureV(actbtt,   			ButtonPos[1], WHITE);
+					DrawTextureV(itembtt,  			ButtonPos[2], WHITE);
+					DrawTextureV(mercybtt, 			ButtonPos[3], WHITE);
+					break;
+				case 1:
+					DrawTextureV(fightbtt, 			ButtonPos[0], WHITE);
+					DrawTextureV(actbtt_selected, 	ButtonPos[1], WHITE);
+					DrawTextureV(itembtt,  			ButtonPos[2], WHITE);
+					DrawTextureV(mercybtt, 			ButtonPos[3], WHITE);
+					break;
+				case 2:
+					DrawTextureV(fightbtt, 			ButtonPos[0], WHITE);
+					DrawTextureV(actbtt,   			ButtonPos[1], WHITE);
+					DrawTextureV(itembtt_selected,  ButtonPos[2], WHITE);
+					DrawTextureV(mercybtt, 			ButtonPos[3], WHITE);
+					break;
+				case 3:
+					DrawTextureV(fightbtt, 			ButtonPos[0], WHITE);
+					DrawTextureV(actbtt,   			ButtonPos[1], WHITE);
+					DrawTextureV(itembtt,  			ButtonPos[2], WHITE);
+					DrawTextureV(mercybtt_selected, ButtonPos[3], WHITE);
+					break;
+				default:
+					break;
+			}
+			DrawTexture(hearttexture, ButtonPos[button_selected].x+8, ButtonPos[button_selected].y+13, RED);
 			break;
-		case 1:
-			DrawTextureV(fightbtt, 			ButtonPos[0], WHITE);
-			DrawTextureV(actbtt_selected, 	ButtonPos[1], WHITE);
-			DrawTextureV(itembtt,  			ButtonPos[2], WHITE);
-			DrawTextureV(mercybtt, 			ButtonPos[3], WHITE);
-			break;
-		case 2:
-			DrawTextureV(fightbtt, 			ButtonPos[0], WHITE);
-			DrawTextureV(actbtt,   			ButtonPos[1], WHITE);
-			DrawTextureV(itembtt_selected,  ButtonPos[2], WHITE);
-			DrawTextureV(mercybtt, 			ButtonPos[3], WHITE);
-			break;
-		case 3:
-			DrawTextureV(fightbtt, 			ButtonPos[0], WHITE);
-			DrawTextureV(actbtt,   			ButtonPos[1], WHITE);
-			DrawTextureV(itembtt,  			ButtonPos[2], WHITE);
-			DrawTextureV(mercybtt_selected, ButtonPos[3], WHITE);
-			break;
-		default:
-			break;
-		}
-		DrawTexture(hearttexture, ButtonPos[button_selected].x+8, ButtonPos[button_selected].y+13, WHITE);
+			case ENEMY:
+				DrawTextureV(fightbtt, 			ButtonPos[0], WHITE);
+				DrawTextureV(actbtt,   			ButtonPos[1], WHITE);
+				DrawTextureV(itembtt,  			ButtonPos[2], WHITE);
+				DrawTextureV(mercybtt, ButtonPos[3], WHITE);
+				DrawTextureV(hearttexture, SoulPos, RED);
+			}
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
